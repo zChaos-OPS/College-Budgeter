505b57e45cf241179b89059952fd6aada6563791
Chandramouli <iyerchandramouli03@gmail.com>
2019-08-11 19:51:05 -0700

    Added the master project

---
+++ Budgeter/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+
+namespace Budgeter
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Button btnScreen2 = FindViewById<Button>(Resource.Id.btnProceedSC1);
+
+            btnScreen2.Click += (sender, e) => {
+                Intent screen2 = new Intent(this, typeof(SC2));
+                StartActivity(screen2);
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
